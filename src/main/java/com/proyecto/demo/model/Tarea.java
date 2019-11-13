d4cc158b9d732b896210f2922638c8c217c50b3f
juliocabrera820 <juliocabrera820@gmail.com>
2019-11-13 13:09:34 -0600

    Validar campos

--- src/main/java/com/proyecto/demo/model/Tarea.java
+++ src/main/java/com/proyecto/demo/model/Tarea.java
@@ -2,6 +2,7 @@ package com.proyecto.demo.model;
 
 import lombok.Data;
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Data
@@ -11,13 +12,16 @@ public class Tarea {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column
+    @NotEmpty(message = "Debes ingresar un nombre")
     private String nombre;
     @Column
+    @NotEmpty(message = "Debes ingresar una descripcion")
     private String descripcion;
     @Column
     private String prioridad;
     @Column
     private String estado;
     @Column
+    @NotEmpty(message = "Debes ingresar un responsable")
     private String responsable;
 }
