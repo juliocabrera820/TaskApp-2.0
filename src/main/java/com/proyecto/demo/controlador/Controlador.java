44220ac5b398e7324415da7d0ba4fc91eef613c1
juliocabrera820 <juliocabrera820@gmail.com>
2019-11-13 13:09:58 -0600

    Validar campos y redirigir al formulario

--- src/main/java/com/proyecto/demo/controlador/Controlador.java
+++ src/main/java/com/proyecto/demo/controlador/Controlador.java
@@ -5,16 +5,26 @@ import com.proyecto.demo.model.TareaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class Controlador {
+
+    private List<String> estados;
+    private List<String> prioridades;
+
+    public Controlador() {
+        estados = Arrays.asList("En espera","En progreso","Terminado");
+        prioridades = Arrays.asList("Alta","Media","Baja");
+    }
 
     @Autowired
     private TareaService tareaService;
@@ -31,15 +41,19 @@ public class Controlador {
         Tarea tarea = new Tarea();
         model.addAttribute("titulo","Crear Tarea");
         model.addAttribute("tarea",tarea);
-        List<String> estados = Arrays.asList("En espera","En progreso","Terminado");
-        List<String> prioridades = Arrays.asList("Alta","Media","Baja");
         model.addAttribute("estados",estados);
         model.addAttribute("prioridades",prioridades);
         return "formTarea";
     }
 
     @PostMapping("/guardar")
-    public String guardar(@ModelAttribute Tarea tarea){
+    public String guardar(@Valid @ModelAttribute Tarea tarea, BindingResult bindingResult,Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("tarea",tarea);
+            model.addAttribute("estados",estados);
+            model.addAttribute("prioridades",prioridades);
+            return "/formTarea";
+        }
         tareaService.agregar(tarea);
         return "redirect:/";
     }
@@ -49,8 +63,6 @@ public class Controlador {
         model.addAttribute("titulo","Editar Tarea");
         Tarea tarea = tareaService.editar(id);
         model.addAttribute("tarea",tarea);
-        List<String> estados = Arrays.asList("En espera","En progreso","Terminado");
-        List<String> prioridades = Arrays.asList("Alta","Media","Baja");
         model.addAttribute("estados",estados);
         model.addAttribute("prioridades",prioridades);
         return "formTarea";
