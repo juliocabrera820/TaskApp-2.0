df6c7c69cdc425f81d7be7ee50256bfdcd5c0f20
juliocabrera820 <juliocabrera820@gmail.com>
2019-11-27 18:34:05 -0600

    Campos sin espacios en blanco

--- src/main/java/com/proyecto/demo/model/entity/Tarea.java
+++ src/main/java/com/proyecto/demo/model/entity/Tarea.java
@@ -3,7 +3,7 @@ package com.proyecto.demo.model.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -16,11 +16,11 @@ public class Tarea {
     private int id;
     @Column
     @Size(max = 50,message = "El nombre debe ser menor a 50 caracteres")
-    @NotEmpty(message = "Debes ingresar un nombre")
+    @NotBlank(message = "Debes ingresar un nombre")
     private String nombre;
     @Column
     @Size(max = 100,message = "La descripcion debe ser menor a 100 caracteres")
-    @NotEmpty(message = "Debes ingresar una descripcion")
+    @NotBlank(message = "Debes ingresar una descripcion")
     private String descripcion;
     @Column
     private String prioridad;
@@ -28,6 +28,6 @@ public class Tarea {
     private String estado;
     @Column
     @Size(max = 50,message = "El responsable debe ser menor a 50 caracteres")
-    @NotEmpty(message = "Debes ingresar un responsable")
+    @NotBlank(message = "Debes ingresar un responsable")
     private String responsable;
 }
