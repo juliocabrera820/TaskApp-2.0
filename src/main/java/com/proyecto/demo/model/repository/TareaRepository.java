dcea711a338dea52d8e56ba23ef560489d2f14ba
juliocabrera820 <juliocabrera820@gmail.com>
2020-02-08 11:54:46 -0600

    renombrar paquetes y componentes

--- src/main/java/com/proyecto/demo/model/repository/TareaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.demo.model.repository;
-
-import com.proyecto.demo.model.entity.Tarea;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TareaRepository extends CrudRepository<Tarea, Integer> {
-}
